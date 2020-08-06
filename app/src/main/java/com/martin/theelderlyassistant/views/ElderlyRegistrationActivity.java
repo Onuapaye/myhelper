df5df32f950b07cbded696361ffaef35704df763
Bismark Atta FRIMPONG <onuapaye@yahoo.com>
2020-08-06 19:26:42 +0200

    created volunteer login/registration activities

---
+++ app/src/main/java/com/martin/theelderlyassistant/views/ElderlyRegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.martin.theelderlyassistant.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.martin.theelderlyassistant.R;
+
+public class ElderlyRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elderly_registration);
+    }
+}
