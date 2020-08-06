35bbf7a7fc1dabc6070bc67696ee091b10d817a5
Bismark Atta FRIMPONG <onuapaye@yahoo.com>
2020-08-06 21:01:13 +0200

    generic method and activity links     1. created a generic method to link activities     2. registation and login activities linked in the respective screens

--- app/src/main/java/com/martin/theelderlyassistant/views/ElderlyRegistrationActivity.java
+++ app/src/main/java/com/martin/theelderlyassistant/views/ElderlyRegistrationActivity.java
@@ -2,7 +2,10 @@ package com.martin.theelderlyassistant.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.martin.theelderlyassistant.R;
 
@@ -12,5 +15,23 @@ public class ElderlyRegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elderly_registration);
+
+        this.openElderlyLoginScreen();
+    }
+
+    /**
+     * Open the elderly login screen when clicked
+     */
+    private void openElderlyLoginScreen(){
+
+        TextView textView = (TextView) findViewById(R.id.loginHereLink);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ElderlyRegistrationActivity.this, ElderlyLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
