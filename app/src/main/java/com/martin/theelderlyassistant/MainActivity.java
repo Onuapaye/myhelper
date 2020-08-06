2edc8c30c2ae74dc5ecb4f7055156c801bb5ad94
Bismark Atta FRIMPONG <onuapaye@yahoo.com>
2020-08-06 20:03:35 +0200

    coded the onclick of the elderly button

--- app/src/main/java/com/martin/theelderlyassistant/MainActivity.java
+++ app/src/main/java/com/martin/theelderlyassistant/MainActivity.java
@@ -2,13 +2,36 @@ package com.martin.theelderlyassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.martin.theelderlyassistant.views.ElderlyLoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.openElderlyLoginScreen();
+    }
+
+    /**
+     * Opens the `Elderly Login` screen when the button of
+     * `I am an Elderly` is clicked
+     */
+    private void openElderlyLoginScreen(){
+        button = (Button) findViewById(R.id.iAmAnElderlyButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ElderlyLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
